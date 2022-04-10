1a1268aa1c2f9f8586d1b3a7754b2e70e115af05
unknown <522289454@qq.com>
2022-04-10 21:55:51 +0800

    '修改httpserver源文件'

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,38 +1,34 @@
 package main
 
-import(
-	"fmt"
-	"os"
-	"net"
-	"net"
-	"net/http"
+import (
 	"io"
+	"log"
+	"net/http"
 )
 
 //健康检查函数
-func healthz(w http.ResponseWriter, r *http.Request) { 
-	  //Fprintf：来格式化并输出到 io.Writers 而不是 os.Stdout。      
-	  io.WriteString(w, "200")
+func healthz(w http.ResponseWriter, r *http.Request) {
+	//Fprintf：来格式化并输出到 io.Writers 而不是 os.Stdout。
+	io.WriteString(w, "200")
 }
 
 //业务处理
-func bussinesshandle(w http.ResponseWriter, r *http.Request){
+func bussinesshandle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("VERSION", "HTTP 2.0")
-	for hk, hv := range r.Header {
-		w.Header().Set(hk, hv)    
+	for hk, _ := range r.Header {
+		w.Header().Set(hk, "hv")
 	}
 	log.Printf("Response code: %d", 200)
 }
 
-
-func main(){
+func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/healthz", healthz)
 	server.HandleFunc("/", bussinesshandle)
-	err := http.ListenAndServe(":8080", nil);
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("start server failed", err)
 	}
-	
+
 	// fmt.Println("hello world")
 }
